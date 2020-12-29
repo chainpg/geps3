d1405c06159b4dcf018b5a963a4c27998fc42fb2
chainpg <chain.li.iisi@gmail.com>
2020-12-29 11:16:43 +0800

    Update dev-main.js

--- dev-main.js
+++ dev-main.js
@@ -646,10 +646,3 @@ menus.forEach(function(vo,i){
 	menuUl.append(ul);
 });
 
-
-if (module.hot) {
-	module.hot.accept('./index.js', function() {
-		console.log('Accepting the updated printMe module!');
-
-	})
-}
